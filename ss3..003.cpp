da4e12f91521619f5484785cde6285ab9329face
giahung123456 <dogiahungtplc@gmail.com>
2024-11-24 23:03:05 +0700

    UPLOAD SS4_03

---
+++ ss3..003.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ main(){
+ 	float r ;
+ 	printf("nhap ban kinh:");
+	 float PI=3.14;
+ 	scanf("%f",&r);
+ 	double chuvi=2*r*PI;
+ 	double dientich=r*r*PI;
+ 	printf ("%.2f\n%.2f",chuvi,dientich);
+ }
